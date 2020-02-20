0c218af2c72f596184b4a132b9c3a54b54c5625a
Steve <drum2093@gmail.com>
2020-02-19 22:53:51 -0500

    Created a button folder.

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import { render, getByText } from '@testing-library/react';
 import React from 'react';
-import Button from 'components/Button';
+import Button from 'components/Button/Button';
 
 describe('Button', () => {
     test('should display text', () => {
