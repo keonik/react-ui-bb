6cf271de61a22ca951ebc540eaea372e9802f936
Stephen Gray <drum2093@gmail.com>
2020-02-22 18:56:53 -0500

    feat(button): Added button size and colors

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+    verbose: true,
     setupFilesAfterEnv: ['./src/setupTests.ts'],
+    moduleFileExtensions: ['js', 'ts', 'tsx'],
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+        '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    },
 };
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,7 +4,7 @@ import styled from '@emotion/styled';
 import { styles } from 'config/styles';
 import { convertStyleObjectToCSS } from 'utils/StyleUtilities';
 
-interface ButtonProps {
+export interface ButtonProps {
     children?: any;
     color: 'primary' | 'secondary' | 'error';
     disabled: boolean;
@@ -19,14 +19,31 @@ interface ButtonProps {
     variant: 'text' | 'outlined' | 'contained';
 }
 
+const float = '2px';
 const Wrapper = styled.button`
     ${convertStyleObjectToCSS(styles.button)};
     font-weight: ${styles.font.fontWeight};
-    ${props => styles.color[`${props.color}`]};
-    ${props => convertStyleObjectToCSS(styles.size[`${props.size}`])}
+    ${(props): string => styles.color[`${props.color}`]};
+    ${(props): string => convertStyleObjectToCSS(styles.size[`${props.size}`])};
+    outline: none;
+    transition: transform 0.5s cubic-bezier(0.1, 1.51, 1, 1.22);
+    &:hover {
+        -webkit-transform: translateY(-${float});
+        transform: translateY(-${float});
+    }
+    &:active {
+        -webkit-transform: translateY(${float});
+        transform: translateY(${float});
+    }
 `;
 
-const Button: React.FC<ButtonProps> = ({ onClick, text, children, color, size }: ButtonProps) => {
+export const Button: React.FC<ButtonProps> = ({
+    onClick,
+    text,
+    children,
+    color,
+    size,
+}: ButtonProps) => {
     return (
         <Wrapper color={color} onClick={onClick} size={size}>
             {children}
@@ -46,5 +63,3 @@ Button.defaultProps = {
     text: '',
     variant: 'contained',
 };
-
-export default Button;
