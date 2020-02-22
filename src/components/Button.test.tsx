b363649e50cd127ed7d0b41b6b727595f5ec8f39
John Fay <john.k.fay@gmail.com>
2020-02-22 18:51:11 -0500

    delete folder

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,6 +1,6 @@
 import { render, getByText } from '@testing-library/react';
 import React from 'react';
-import Button from 'components/button/Button';
+import { Button } from 'components';
 
 describe('Button', () => {
     test('should display text', () => {
